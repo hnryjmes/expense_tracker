4acf07282e6c41d9d6cd72e94400e708154f7d11
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-14 15:16:19 +0100

    add route

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -15,26 +15,27 @@ module ExpenseTracker
       expect(last_response.status).to eq(200)
 
       parsed = JSON.parse(last_response.body)
-      expect(parsed).to include('expense_id' => a_kind_of(Integer)) expense.merge('id' => parsed['expense_id'])
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
     end
 
     it 'records submitted expenses' do
       coffee = post_expense(
-        'payee' => 'Starbucks',
+        'payee'  => 'Starbucks',
         'amount' => 5.75,
-        'date' => '2017-06-10'
+        'date'   => '2017-06-10'
       )
 
       zoo = post_expense(
-        'payee' => 'Zoo',
+        'payee'  => 'Zoo',
         'amount' => 15.25,
-        'date' => '2017-06-10'
+        'date'   => '2017-06-10'
       )
 
       groceries = post_expense(
-        'payee' => 'Whole Foods',
+        'payee'  => 'Whole Foods',
         'amount' => 95.20,
-        'date' => '2017-06-11'
+        'date'   => '2017-06-11'
       )
       # POST coffee, zoo, and groceries expenses here
 
