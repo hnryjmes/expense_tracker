3b8ae999e9499cf4e1060c0a8db2c4683e4f81ce
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-17 09:57:49 +0100

    update

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -21,7 +21,7 @@ module ExpenseTracker
 
     it 'records submitted expenses' do
       pending 'Need to persist expenses'
-      
+
       coffee = post_expense(
         'payee'  => 'Starbucks',
         'amount' => 5.75,
