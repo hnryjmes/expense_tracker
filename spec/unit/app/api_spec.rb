17ffe46f313e8e279883e67999e263fa80b7e2e8
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-17 11:08:48 +0100

    update api spec

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -14,39 +14,33 @@ module ExpenseTracker
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
 
     describe 'POST /expenses' do
-
-      context 'when the expense fails validation' do
-        it 'returns an error message'
-        it 'responds with a 422 (Unprocessable entity)'
-      end
-
       context 'when the expense is successfully recorded' do
-        it 'returns the expense id' do
-          expense = { 'some' => 'data' }
+        let(:expense) { { 'some' => 'data' } }
 
+        before do
           allow(ledger).to receive(:record)
             .with(expense)
             .and_return(RecordResult.new(true, 417, nil))
-
-            post '/expenses', JSON.generate(expense)
-
-            parsed = JSON.parse(last_response.body)
-            expect(parsed).to include('expense_id' => 417)
         end
+
+        it 'returns the expense id' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('expense_id' => 417)
+        end
+
         it 'responds with a 200 (OK)' do
-          expense = { 'some' => 'data' }
-
-          allow(ledger).to receive(:record)
-            .with(expense)
-            .and_return(RecordResult.new(true, 417, nil))
-
           post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(200)
         end
       end
 
-      # ... next context will go here...
-
+      context 'when the expense fails validation' do
+        # ... specs go here ...
+        it 'returns an error message'
+        it 'responds with a 422 (Unprocessable entity)'
+      end
     end
   end
 end
